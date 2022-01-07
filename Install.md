5c4f3e5375ae0c5eaa5f3860c3b65506664f8447
Mauricio Arrechea <mauricioarrechea@pop-os.localdomain>
2022-01-07 10:46:40 +0100

    changes to install.md and some tweaks for the script

--- Install.md
+++ Install.md
@@ -1,9 +1,13 @@
 Requirements : Docker, Docker-Compose, Twig, Doctrine
 
+Clone the repository: 
+```
 git clone git@github.com:marreche/castle.git
+```
+Run config.sh:
+```
+bash config.sh
+```
+Follow the instructions displayed on your terminal.
 
-sudo docker-compose up -d
-
-composer require symfony/runtime
-
-sudo symfony serve -d
+ENJOY!
--- config.sh
+++ config.sh
@@ -7,24 +7,28 @@ echo "HELLO $(whoami) and WELCOME TO MAURICIO ARRECHEA'S STRONGHOLD TECHNICAL TE
 echo "Please wait while some configurations are made for the project to work"
 echo "--------------------------------------------------"
 echo "Configuring Docker..."
-docker-compose up -d
-echo "${GREEN}Docker Configured!"
+sudo docker-compose up -d
+echo -e "${GREEN}Docker Configured!"
 echo "Installing dependencies..."
 composer require symfony/runtime
-echo "${GREEN}Installation complete!${NC}"
+echo -e "${GREEN}Installation complete!${NC}"
 echo "Launching server..."
 sudo symfony serve -d
-echo "${GREEN}Server running at localhost!${NC}"
-echo "Copy the query below and insert in the mysql cli: \
-CREATE TABLE projects ( \
-id INT AUTO_INCREMENT NOT NULL, \
-name VARCHAR(255) NOT NULL, \
-amount BIGINT NOT NULL, \
-start_date DATETIME NOT NULL, \
-PRIMARY KEY(id)) \
-DEFAULT CHARACTER SET utf8mb4 COLLATE 'utf8mb4_unicode_ci' ENGINE = InnoDB;"
-echo "to exit the mysql cli just type ${RED}'exit'${NC}"
-docker-compose exec database mysql --password=password
+echo -e "${GREEN}Server running at localhost!${NC}"
+echo "Instructions for MySQL cli"
+echo "USE main;"
+echo "Copy the query below and insert in the mysql cli: "
+echo 
+echo "CREATE TABLE projects ( "
+echo "id INT AUTO_INCREMENT NOT NULL, "
+echo "name VARCHAR(255) NOT NULL, "
+echo "amount BIGINT NOT NULL, "
+echo "start_date DATETIME NOT NULL, "
+echo "PRIMARY KEY(id))" 
+echo "DEFAULT CHARACTER SET utf8mb4 COLLATE 'utf8mb4_unicode_ci' ENGINE = InnoDB;"
+echo 
+echo -e "To exit the mysql cli just type ${RED}'exit'${NC}"
+sudo docker-compose exec database mysql --password=password
 
 
 
